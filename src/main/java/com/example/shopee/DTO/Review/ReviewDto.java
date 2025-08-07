9b4159769877d53e29dd17a52d83af7defcce4d9
Yash Rajeshwar Shirbhate <yashshirbhatesl@gmail.com>
2025-08-07 20:56:47 +0530

    Review Backend Apis

--- src/main/java/com/example/shopee/DTO/Review/ReviewDto.java
+++ src/main/java/com/example/shopee/DTO/Review/ReviewDto.java
@@ -1,6 +1,7 @@
 package com.example.shopee.DTO.Review;
 
 import com.example.shopee.Model.Review;
+import jakarta.validation.constraints.*;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,13 +10,30 @@ import lombok.Setter;
 @Setter
 public class ReviewDto {
     private Long id;
+
+    @NotNull(message = "productId is required")
     private Long productId;
+
+    @NotNull(message = "customerId is required")
     private Long customerId;
+
+    @NotNull(message = "orderId is required")
     private Long orderId;
+
+    @NotNull(message = "rating is required")
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 5, message = "Rating must be at most 5")
     private Integer rating;
+
+    @NotBlank(message = "reviewTitle is required")
     private String reviewTitle;
+
+    @NotBlank(message = "reviewText is required")
     private String reviewText;
+
     private String imageUrls;
-    private Boolean isVerifiedPurchase;
-    private Review.Status status;
+
+    private Boolean isVerifiedPurchase = false;
+
+    private Review.Status status = Review.Status.PENDING;
 }
